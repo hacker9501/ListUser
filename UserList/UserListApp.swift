34d104a25c581cf45f447ded6d381a53c5f40658
Elver <holahacker01@gmail.com>
2024-01-03 00:40:32 -0500

    Initial Commit

---
+++ UserList/UserListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserListApp.swift
+//  UserList
+//
+//  Created by Elver Mayta Hernández on 3/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UserListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
